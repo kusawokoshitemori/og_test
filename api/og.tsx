2632a209e1391fb586b666782b1465d4fa904ad6
kusawokoshitemori <kyou0317kyou0317@gmail.com>
2025-05-31 22:48:28 +0900

    vercelのプレビュー環境で動作確認するためのテスト

---
+++ api/og.tsx
@@ -0,0 +1,12 @@
+import { ImageResponse } from "@vercel/og";
+
+export const config = { runtime: "edge" };
+
+export default async function handler() {
+  console.log("OG handler start");
+  console.log("OG handler before return");
+  return new ImageResponse(<div>Hello</div>, {
+    width: 1200,
+    height: 630,
+  });
+}
