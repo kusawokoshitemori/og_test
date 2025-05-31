ef5f7e45d1c4b447cc94cf42e59fb970dbafc20b
kusawokoshitemori <kyou0317kyou0317@gmail.com>
2025-05-31 23:44:28 +0900

    test: OG画像のテスト

--- api/og.tsx
+++ api/og.tsx
@@ -1,13 +1,27 @@
 import { ImageResponse } from "@vercel/og";
 
-export const config = { runtime: "edge" };
-
 export default async function handler() {
-  console.log("OG handler start");
-  console.log("OG handler before return");
-  return new ImageResponse(<div>Hello</div>, {
-    width: 1200,
-    height: 630,
-  });
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: "black",
+          background: "white",
+          width: "100%",
+          height: "100%",
+          padding: "50px 200px",
+          textAlign: "center",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        👋 Hello
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
 }
-// test用コメントアウト
