9c66433aeedc1831c5b5593427b92e06c9cce358
kusawokoshitemori <kyou0317kyou0317@gmail.com>
2025-06-19 18:51:48 +0900

    Claude君がくれたコードどうりに実装

--- api/og.tsx
+++ api/og.tsx
@@ -1,52 +1,33 @@
 import { ImageResponse } from "@vercel/og";
-// App router includes @vercel/og.
-// No need to install it.
 
-async function loadGoogleFont(font: string, text: string) {
-  const url = `https://fonts.googleapis.com/css2?family=${font}&text=${encodeURIComponent(
-    text
-  )}`;
-  const css = await (await fetch(url)).text();
-  const resource = css.match(
-    /src: url\((.+)\) format\('(opentype|truetype)'\)/
-  );
-
-  if (resource) {
-    const response = await fetch(resource[1]);
-    if (response.status == 200) {
-      return await response.arrayBuffer();
-    }
-  }
-
-  throw new Error("failed to load font data");
-}
-
-export async function GET() {
-  const text = "Hello world!";
-
+export default async function handler(req: Request) {
   return new ImageResponse(
     (
       <div
         style={{
-          backgroundColor: "white",
-          height: "100%",
+          fontSize: 128,
+          background: "white",
           width: "100%",
-          fontSize: 100,
-          fontFamily: "Geist",
-          paddingTop: "100px",
-          paddingLeft: "50px",
+          height: "100%",
+          display: "flex",
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        {text}
+        Hello world!
       </div>
     ),
     {
       width: 1200,
-      height: 630,
+      height: 600,
       fonts: [
         {
-          name: "Geist",
-          data: await loadGoogleFont("Geist", text),
+          name: "Noto Sans JP",
+          data: await fetch(
+            new URL("./NotoSans-Regular.ttf", import.meta.url)
+          ).then((res) => res.arrayBuffer()),
+          weight: 400,
           style: "normal",
         },
       ],
